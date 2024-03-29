f826f4406ee792f9ceb1b5da80e6590c33cf71c2
Kn62 <knlecocq@gmail.com>
2024-03-29 02:00:02 +0100

    adding a word list to the game to choose a random word in it

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let playGame = function() {
-    let misteryWord = prompt("Choose the mistery word").toUpperCase().split('');
+
+    const getRandomWord = function () {
+        const wordList = ["Aatrox", "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "Aurelion Sol", "Azir",
+                          "Bard", "Bel'Veth", "Blitzcrank", "Brand", "Braum", "Briar", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki",
+                          "Darius", "Diana", "Dr.Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio",
+                          "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger", "Hwei", "Illaoi", "Irelia", "Ivern",
+                          "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", "K'Sante", "Kai'sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina",
+                          "Kayle", "Kayn", "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "Leblanc", "Lee Sin", "Leona", "Lilia", "Lissandra", "Lucian",
+                          "Lulu", "Lux", "Maitre Yi", "Malphite", "Malzahar", "Maokai", "Milio", "Miss Fortune", "Mordekaiser", "Morgana", "Naafiri", "Nami",
+                          "Nasus", "Nautilus", "Neeko", "Nidalee", "Nilah", "Nocturne", "Nunu et Willump", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy",
+                          "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek'Sai", "Rell", "Renata Glasc", "Renekton", "Rengar", "Riven", "Rumble", "Ryze",
+                          "Samira", "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Smolder", "Sona",
+                          "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere",
+                          "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vex", "Vi", "Viego", "Viktor", "Vladimir", "Volibear",
+                          "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", "Zyra"];
+        let randomIndex = Math.floor( Math.random() * wordList.length);
+        return wordList[randomIndex];
+    }
+
+    let misteryWord = getRandomWord().toUpperCase().split('');
     let display = "_".repeat(misteryWord.length).split('');
     let numberOfTry = 0;
     
